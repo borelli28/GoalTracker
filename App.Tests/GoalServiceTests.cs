cee718a15e1da5a8c8d46d02a4e2a9cff7cb4104
Armando <borelliarmando28@gmail.com>
2024-07-07 11:55:26 -0400

    Added setup and teardown for GoalServiceTests

--- App.Tests/GoalServiceTests.cs
+++ App.Tests/GoalServiceTests.cs
@@ -1,1 +1,36 @@
+using NUnit.Framework;
+using App.Services;
+using App.Models;
+using App.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace App.UnitTests.Services
+{
+    [TestFixture]
+    public class GoalServiceTests
+    {
+        private AppDbContext _context;
+        private IGoalService _goalService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestGoalServiceDB")
+                .Options;
+
+            _context = new AppDbContext(options);
+            _jobService = new JobService(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+    }
+}
