07ed31e2bcd624ddec9e65c94e90f0c83ff42349
Armando <borelliarmando28@gmail.com>
2024-08-02 11:45:24 -0400

    Added create goal component to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
+import CreateGoalForm from './components/CreateGoal';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       </nav> */}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateGoalForm />} />
       </Routes>
     </Router>
   );
